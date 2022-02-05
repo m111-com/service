5dce074121335540d1ba6447e27754bde1e6a075
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-02-06 02:39:09 +0500

    test: import in root file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import 'dotenv/config';
 import express, { Application, Request, Response } from 'express';
+import logger from './utils/logger';
 
 // Boot express
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
-// Application routing
 app.use('/', (req: Request, res: Response) => {
 	res.status(200).send({ data: 'Hello from m111' });
 });
 
-// Start server
-app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+app.listen(port, () => logger.info(`Server is listening on port ${port}!`));
